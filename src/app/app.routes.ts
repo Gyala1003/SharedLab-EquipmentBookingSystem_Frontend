9ce1c6c7d585154e06737cf1252d2256f9ed440a
VuongK4 <vuongddhe1186713@fpt.edu.vn>
2026-07-16 17:10:19 +0700

    hoan thien thu vien

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { PublicLayoutComponent } from './shared/layout/public-layout/public-layout.component';
-import { HomeComponent } from './feature/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
     {
